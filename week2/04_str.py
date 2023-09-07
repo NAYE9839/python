4ee6ceedbdc844f682f98153acc67ee25a1a20e9
NAYE <128660854+NAYE9839@users.noreply.github.com>
2023-09-07 19:15:27 +0900

    Create 04_str.py

---
+++ week2/04_str.py
@@ -0,0 +1,33 @@
+"""
+안녕 나는 문자열 표현리터럴이야
+하지만
+여러 줄 주석인척 하지!
+"""
+
+
+
+
+print(
+"""\
+a
+a"""
+)
+
+print(
+    """
+a
+a"""
+)
+
+print(
+    """a
+a"""
+)
+
+print("a'a'")
+print('"a"a')
+print('"a" \'a\'')
+
+print("aa")
+print('aa')
+#print("aa')
