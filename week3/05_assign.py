14f213a09fd59e28c8a1cd59becead7c0cc2d8d4
NAYE <128660854+NAYE9839@users.noreply.github.com>
2023-09-14 18:40:47 +0900

    Create 05_assign.py

---
+++ week3/05_assign.py
@@ -0,0 +1,16 @@
+#복합대입연산자 p.116
+
+number = 10
+number += 10
+number -= 4
+number *= 2
+number /= 4
+number %= 10
+number **= 2
+print(number)
+
+# p.117
+number = "10"
+number += "b"
+number *= 10
+print(number)
