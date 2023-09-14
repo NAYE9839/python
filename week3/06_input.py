1c3977a06668d0db62108324260d388db9d212d0
NAYE <128660854+NAYE9839@users.noreply.github.com>
2023-09-14 19:15:04 +0900

    Create 06_input.py

---
+++ week3/06_input.py
@@ -0,0 +1,13 @@
+age = int(input("올해 나이:"))
+print("내년 나이:" , str(age + 1))
+
+age = input("나이:")
+print(age + "1")
+print(int(age) + 1)
+
+age = int(input("나이:"))
+print(age + 1)
+
+age = input("나이:")
+age = int(age)
+print = (age + 1)
