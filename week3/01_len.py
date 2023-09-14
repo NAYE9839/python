444b37f541e204c8e02bdd780acbe7e36f03582d
NAYE <128660854+NAYE9839@users.noreply.github.com>
2023-09-14 18:38:21 +0900

    Create 01_len.py

---
+++ week3/01_len.py
@@ -0,0 +1,18 @@
+my_school="인하공전"
+length=len(my_school)
+print(length)
+
+length=len(my_school)
+sub_str=my_school[1:length]
+print(str)
+
+# length=len(my_school)
+# print(my_school[1:length])
+
+# print(my_school[1:len(my_school)])
+
+
+# a="inha"
+# print(a[1]) #0부터 시작, n출력
+# print(a[2:]) #2부터 끝까지
+# print(a[2:4]) #2부터 4전까지
